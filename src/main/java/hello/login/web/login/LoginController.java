31caaed2db5b7ac58d1332715315b0ab364b8d74
CJ <choijun0627@konkuk.ac.kr>
2023-08-10 21:16:14 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 6
    로그인, 로그아웃 기능 쿠키 이용
    
    로그인에 성공 시 서버에서 쿠키 만들어 response에 넣어 보내는 로직을
    LoginController에 login 함수에 추가
    홈 화면에서 쿠키가 있으면 사용자 이름 보이는 화면으로 이동하도록
    HomeController에 homeLogin 함수 추가
    로그인 시 홈 화면인 loginHome.html 생성

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -24,17 +24,34 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
+
         Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
         log.info("login? {}", loginMember);
+
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
-        //로그인 성공 처리 TODO
+
+        //로그인 성공 처리
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
