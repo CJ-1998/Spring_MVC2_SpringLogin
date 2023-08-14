81999ba8091416f97c15a54210e599501aef2297
CJ <choijun0627@konkuk.ac.kr>
2023-08-14 19:23:28 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 7
    서블릿 필터 어플리케이션에 적용
    
    로그인한 사용자만 상품 관리 등에 들어갈 수 있는 필터인 LoginCheckFilter 생성
    WebConfig에 LoginCheckFilter bean으로 등록
    LoginController에 로그인 시 다시 이전 봤던 페이지로 리다이렉트 해주는 기능 추가한 loginV4 추가

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -66,7 +66,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -86,6 +86,33 @@ public class LoginController {
         //세션에 로그인 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
+    }
+
+    /**
+     * 로그인 이후 redirect 처리
+     */
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        //redirectURL 적용
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("/logout")
